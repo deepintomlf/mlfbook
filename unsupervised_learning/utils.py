a59878fe6fe642aff35f30a39c60ed8c14d7e3f0
xd610 <xindong@xins-mbp.mynet>
2019-04-14 21:41:09 +0100

    add utils file: add scaler

---
+++ unsupervised_learning/utils.py
@@ -0,0 +1,15 @@
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import Normalizer
+
+def data_scaler(data,scaler_type):
+    if scaler_type == 'standard':
+        transformed_data = StandardScaler().fit_transform(data)
+    elif scaler_type == 'minmax':
+        transformed_data = MinMaxScaler().fit_transform(data)
+    elif scaler_type == 'normalize':
+        transformed_data = Normalizer.fit_transform(data)
+    else:
+        transformed_data = None
+    return transformed_data
+
